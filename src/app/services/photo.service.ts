6be776c0f688515c5a4c19587feb51709d28d6aa
Michael David Machacuay <mdavidmachacuay@gmail.com>
2023-08-14 23:39:58 -0500

    Loading photos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -8,6 +8,7 @@ import { Preferences } from '@capacitor/preferences';
 })
 export class PhotoService {
   public photos: UserPhoto[] = [];
+  private PHOTO_STORAGE: string = 'photos';
 
   constructor() { }
 
@@ -22,6 +23,11 @@ export class PhotoService {
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
+
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos),
+    });
   }
 
   private async savePicture(photo: Photo) {
@@ -60,6 +66,24 @@ export class PhotoService {
     };
     reader.readAsDataURL(blob);
   });
+
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const { value } = await Preferences.get({ key: this.PHOTO_STORAGE });
+    this.photos = (value ? JSON.parse(value) : []) as UserPhoto[];
+
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+
+      // Web platform only: Load the photo as base64 data
+      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
+  }
 }
 
 export interface UserPhoto {
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -15,6 +15,10 @@ export class Tab2Page {
 
   constructor(public photoService: PhotoService) {}
 
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
